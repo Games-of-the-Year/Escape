fc4365437d6f255f9d51eaa6bd701dcd46fc3403
高見怜那 <111954145+AaiEa00@users.noreply.github.com>
2023-10-04 15:03:34 +0900

    Add: editorconfig
    エディターの設定を統一するやつ

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;    // X方向に移動する量
+    private float movementZ;    // Z方向に移動する量
+    private float movementSpeed;// 移動速度
+    private Vector3 movement;   // 移動量
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        movementSpeed = 10f;
+    }
+
+    void FixedUpdate()
+    {
+        movement.Normalize();   // 移動量の正規化
+
+        rb.AddForce(movement * movementSpeed);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 move = context.ReadValue<Vector2>();
+
+        movementX = move.x;
+        movementZ = move.y;
+
+        movement = new Vector3(movementX, 0, movementZ);
+    }
+}
