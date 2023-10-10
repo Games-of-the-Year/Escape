cc8de0a484421491df534f7056dd4e867c510857
AaiEa00 <oky2284861@stu.o-hara.ac.jp>
2023-10-10 23:44:32 +0900

    Update: PlayerController
    Input System使用可能に

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,61 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private float movementX;    // X方向に移動する量
-    private float movementZ;    // Z方向に移動する量
-    private float movementSpeed;// 移動速度
-    private Vector3 movement;   // 移動量
+    private PlayerInput input;
 
-    void Start()
+    private Vector3 movement;
+    private bool isMoving;
+
+    [SerializeField] private float speed = 10f;
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        TryGetComponent(out rb);
+        TryGetComponent(out input);
+    }
 
-        movementSpeed = 10f;
+    private void OnEnable()
+    {
+        input.actions["Move"].performed += OnMove;
+        input.actions["Move"].canceled += OnMoveStop;
+    }
+
+    private void OnDisable()
+    {
+        input.actions["Move"].performed -= OnMove;
+        input.actions["Move"].canceled -= OnMoveStop;
+    }
+
+    private void OnMoveStop(InputAction.CallbackContext context)
+    {
+        // ボタンを離した時に呼ばれて、移動を止める
+        movement = Vector3.zero;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        var value = context.ReadValue<Vector2>();
+        Debug.Log(value);
+        movement = new Vector3(value.x, 0, value.y);
     }
 
     void FixedUpdate()
     {
-        movement.Normalize();   // 移動量の正規化
-
-        rb.AddForce(movement * movementSpeed);
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        Vector2 move = context.ReadValue<Vector2>();
-
-        movementX = move.x;
-        movementZ = move.y;
-
-        movement = new Vector3(movementX, 0, movementZ);
+        if (movement == Vector3.zero)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            rb.AddForce(movement * speed);
+        }
     }
 }
