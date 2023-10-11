7d33f834ad0746c1c5223bccf39d0eae27b037bd
高見怜那 <111954145+AaiEa00@users.noreply.github.com>
2023-10-11 13:39:21 +0900

    Update GameManager.cs
    変数追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    // Local Multiplayer
+    [SerializeField] private GameObject playerPrefab;
+    private int numberOfPlayers = 2;
+
     void Start()
     {
         
