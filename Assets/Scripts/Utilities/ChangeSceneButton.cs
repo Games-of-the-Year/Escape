510cbf637d7f4284b8ef1c6bbb93ceefccdabb8b
高見怜那 <111954145+AaiEa00@users.noreply.github.com>
2023-11-13 10:27:08 +0900

    更新 シーン切り替え
    とりあえずシーンが切り替えられるようになった。

---
+++ Assets/Scripts/Utilities/ChangeSceneButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+    [SerializeField] private int sceneToChangeTo = 0;
+    [SerializeField] Vector3 spawnPoint = new Vector3(0, 0, 0);
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+
+        GameObject nagito = GameObject.Find("Nagito2(Clone)");
+        if (nagito != null)
+        {
+            nagito.transform.position = spawnPoint;
+        }
+    }
+}
